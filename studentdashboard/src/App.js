e98fcaef7560f79c08e38ce8c1b2eb53f6c7b83d
Raimon Leeuwenburg <r.leeuwenburg@gmail.com>
2020-03-12 10:36:54 +0100

    react app setup

---
+++ studentdashboard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Winc Academy project W6D5: Student Dashboard</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
